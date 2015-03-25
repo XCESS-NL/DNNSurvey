d6cf6c45937ccdb81bb0b3bb9a603c0db0e330e1
Serge van der Ven <serge.van.der.ven@xcess.nl>
2015-03-25 12:24:28 +0100

    Initial commit

---
+++ Controllers/SurveyController.cs
@@ -0,0 +1,49 @@
+﻿#region Copyright
+// DotNetNuke® - http://www.dnnsoftware.com
+// Copyright (c) 2002-2014
+// by DNN Corporation
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+// documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+// the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+// to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+// of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+// DEALINGS IN THE SOFTWARE.
+#endregion
+
+namespace Dnn.Modules.Survey.Controllers
+{
+    using DotNetNuke.Data;
+    using DotNetNuke.Web.Mvc.Framework.ActionFilters;
+    using DotNetNuke.Web.Mvc.Framework.Controllers;
+
+    [DnnHandleErrorAttribute]
+    public class SurveyController : DnnController
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SurveyController"/> class.
+        /// </summary>
+        public SurveyController() 
+            : this(DotNetNuke.Data.DataContext.Instance())
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SurveyController"/> class.
+        /// </summary>
+        /// <param name="dataContext">The data context.</param>
+        public SurveyController(IDataContext dataContext)
+        {
+            this.DataContext = dataContext;
+        }
+
+        protected IDataContext DataContext { get; private set; }
+    }
+}
